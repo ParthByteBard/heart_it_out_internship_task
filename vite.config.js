8e74c41b40f3c6a1d03e5f4e02064c1d6170f8d6
ParthByteBard <parthsatvekar2004@gmail.com>
2024-11-29 18:22:33 +0530

    removed the dist folder

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/heart_it_out_internship_task/', // Add this line
 })
